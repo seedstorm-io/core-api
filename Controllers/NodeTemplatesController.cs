cb4631eadd2cf55c2c095294943a8034519fa712
Clint.Network <me@clint.network>
2019-03-23 03:23:00 +0100

    Add some controllers

---
+++ Controllers/NodeTemplatesController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SeedStorm.CoreApi;
+using SeedStorm.CoreApi.Entities.NodeCatalog;
+
+namespace core_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NodeTemplatesController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public NodeTemplatesController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/NodeTemplates
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<NodeTemplate>>> GetNodesCatalog()
+        {
+            return await _context.NodesCatalog.ToListAsync();
+        }
+
+        // GET: api/NodeTemplates/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<NodeTemplate>> GetNodeTemplate(Guid id)
+        {
+            var nodeTemplate = await _context.NodesCatalog.FindAsync(id);
+
+            if (nodeTemplate == null)
+            {
+                return NotFound();
+            }
+
+            return nodeTemplate;
+        }
+
+        // PUT: api/NodeTemplates/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutNodeTemplate(Guid id, NodeTemplate nodeTemplate)
+        {
+            if (id != nodeTemplate.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(nodeTemplate).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NodeTemplateExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/NodeTemplates
+        [HttpPost]
+        public async Task<ActionResult<NodeTemplate>> PostNodeTemplate(NodeTemplate nodeTemplate)
+        {
+            _context.NodesCatalog.Add(nodeTemplate);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetNodeTemplate", new { id = nodeTemplate.Id }, nodeTemplate);
+        }
+
+        // DELETE: api/NodeTemplates/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<NodeTemplate>> DeleteNodeTemplate(Guid id)
+        {
+            var nodeTemplate = await _context.NodesCatalog.FindAsync(id);
+            if (nodeTemplate == null)
+            {
+                return NotFound();
+            }
+
+            _context.NodesCatalog.Remove(nodeTemplate);
+            await _context.SaveChangesAsync();
+
+            return nodeTemplate;
+        }
+
+        private bool NodeTemplateExists(Guid id)
+        {
+            return _context.NodesCatalog.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Controllers/NodesController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SeedStorm.CoreApi;
+using SeedStorm.CoreApi.Entities.Node;
+
+namespace core_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NodesController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public NodesController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Nodes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Node>>> GetNodes()
+        {
+            return await _context.Nodes.ToListAsync();
+        }
+
+        // GET: api/Nodes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Node>> GetNode(Guid id)
+        {
+            var node = await _context.Nodes.FindAsync(id);
+
+            if (node == null)
+            {
+                return NotFound();
+            }
+
+            return node;
+        }
+
+        // PUT: api/Nodes/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutNode(Guid id, Node node)
+        {
+            if (id != node.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(node).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NodeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Nodes
+        [HttpPost]
+        public async Task<ActionResult<Node>> PostNode(Node node)
+        {
+            _context.Nodes.Add(node);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetNode", new { id = node.Id }, node);
+        }
+
+        // DELETE: api/Nodes/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Node>> DeleteNode(Guid id)
+        {
+            var node = await _context.Nodes.FindAsync(id);
+            if (node == null)
+            {
+                return NotFound();
+            }
+
+            _context.Nodes.Remove(node);
+            await _context.SaveChangesAsync();
+
+            return node;
+        }
+
+        private bool NodeExists(Guid id)
+        {
+            return _context.Nodes.Any(e => e.Id == id);
+        }
+    }
+}
