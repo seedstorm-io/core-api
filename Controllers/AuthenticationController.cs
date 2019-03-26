618e09ac3375653ba64796979faa7c69cd7455a6
Clint.Network <me@clint.network>
2019-03-26 20:31:04 +0100

    Implement Authentication Controller

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using SeedStorm.Core.Dtos;
+using SeedStorm.Core.Entities;
+
+namespace seedstorm_core.Controllers
+{
+    [Authorize]
+    [ApiVersion("1.0")]
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationController(IConfiguration configuration, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public async Task<ActionResult> AuthenticateAsync([FromBody]AuthDto AuthDto)
+        {
+            var user = await _userManager.FindByEmailAsync(AuthDto.Email);
+            if (user != null)
+            {
+                var response = await _signInManager.CheckPasswordSignInAsync(user, AuthDto.Password, false);
+                if (response.Succeeded)
+                {
+                    var tokenHandler = new JwtSecurityTokenHandler();
+                    var tokenKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+                    var tokenDescriptor = new SecurityTokenDescriptor
+                    {
+                        Subject = new ClaimsIdentity(new[]
+                        {
+                            new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+                            new Claim(ClaimTypes.Email, user.Email),
+                        }),
+                        IssuedAt = DateTime.Now,
+                        NotBefore = DateTime.Now,
+                        Expires = DateTime.MaxValue,
+                        Audience = _configuration["Jwt:Issuer"],
+                        Issuer = _configuration["Jwt:Issuer"],
+                        SigningCredentials = new SigningCredentials(tokenKey, SecurityAlgorithms.HmacSha256),
+                        //EncryptingCredentials = new EncryptingCredentials(tokenKey, JwtConstants.DirectKeyUseAlg, SecurityAlgorithms.Aes256CbcHmacSha512)
+                    };
+                    var token = tokenHandler.CreateToken(tokenDescriptor);
+                    var tokenString = tokenHandler.WriteToken(token);
+                    return Ok(new
+                    {
+                        user.Email,
+                        user.AccessFailedCount,
+                        Token = tokenString
+                    });
+                }
+                else if (response.IsNotAllowed)
+                {
+                    return Unauthorized(new { message = "Username or password is incorrect" });
+                }
+                else if (response.RequiresTwoFactor)
+                {
+                    return BadRequest("Two factor is required");
+                }
+                else if (response.IsLockedOut)
+                {
+                    return BadRequest("is locked");
+                }
+            }
+            return BadRequest(new { message = "This user does not exist" });
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<ActionResult> RegisterAsync([FromBody]AuthDto AuthDto)
+        {
+            var user = new ApplicationUser()
+            {
+                Email = AuthDto.Email,
+                UserName = AuthDto.Email,
+                IsAllowed = true
+            };
+            var response = await _userManager.CreateAsync(user, AuthDto.Password);
+            if(response.Succeeded)
+            {
+                return Ok();
+            }
+            return BadRequest(new { errors = response.Errors });
+        }
+
+        [HttpGet("get")]
+        public async Task<ActionResult<ApplicationUser>> GetAsync()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return await GetAsync(userId);
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpGet("get/{id}")]
+        public async Task<ActionResult<ApplicationUser>> GetAsync(string id)
+        {
+            return await _userManager.FindByIdAsync(id);
+        }
+        
+        [Authorize(Roles = "Administrator")]
+        [HttpDelete("delete")]
+        public async Task<ActionResult> DeleteAsync([FromBody]AuthDto AuthDto)
+        {
+            var user = await _userManager.FindByEmailAsync(AuthDto.Email);
+            var response = await _userManager.DeleteAsync(user);
+            if (response.Succeeded)
+            {
+                return Ok();
+            }
+            return BadRequest(new { errors = response.Errors });
+        }
+
+    }
+}
