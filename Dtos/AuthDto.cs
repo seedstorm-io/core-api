7cdc4cec3acfc796b70b62e18624480d06ed3fbf
Clint.Network <me@clint.network>
2019-03-26 20:30:52 +0100

    Add AuthDto

---
+++ Dtos/AuthDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeedStorm.Core.Dtos
+{
+    public class AuthDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
