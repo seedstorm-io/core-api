a9da6553ff62926258a588d8fb0480bad198f688
Clint.Network <me@clint.network>
2019-04-20 19:40:18 +0200

    Add Full name

--- Entities/ApplicationUser.cs
+++ Entities/ApplicationUser.cs
@@ -8,6 +8,7 @@ namespace SeedStorm.Core.Entities
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FullName { get; set; }
         public DateTime RegisterDatetime { get; set; }
         public bool IsAllowed { get; set; }
     }
