54a945293d99e88fd330b76fe28154709d3aacad
Clint.Network <me@clint.network>
2019-04-20 19:40:30 +0200

    Add News Model

---
+++ Entities/News.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeedStorm.Core.Entities
+{
+    public class News
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+    }
+}
