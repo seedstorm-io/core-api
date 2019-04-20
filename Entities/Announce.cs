4530049ba03f7102f0f29e8df0358f228ee9d9e6
Clint.Network <me@clint.network>
2019-04-20 19:40:07 +0200

    Create Announce Model

---
+++ Entities/Announce.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeedStorm.Core.Entities
+{
+    public class Announce
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ActionButton { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
